2b95152993bb3f90ea15f85a951f3c9a49971f57
Eric Sullivan <eric.sullivan@annkissam.com>
2020-09-28 16:54:59 -0400

    add ex_doc

--- mix.exs
+++ mix.exs
@@ -14,7 +14,7 @@ defmodule DepMulti.MixProject do
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
-      description: "Ecto.Multi + Task.async",
+      description: "Task.async with depenencies / Ecto.Multi syntax",
       package: package(),
       source_url: @url,
       homepage_url: @url,
@@ -34,8 +34,7 @@ defmodule DepMulti.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
     ]
   end
 
